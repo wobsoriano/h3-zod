2dbe6393456c208c325fb800c00fcf999a1cb08d
Robert Soriano <sorianorobertc@gmail.com>
2022-06-27 19:50:52 -0700

    add utilities

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,50 @@
-export const one = 1
-export const two = 2
+import type { CompatibilityEvent } from 'h3'
+import { createError, useBody, useQuery } from 'h3'
+import { z } from 'zod'
+
+// copy of the private Zod utility type of ZodObject
+type UnknownKeysParam = 'passthrough' | 'strict' | 'strip'
+
+type TQuery<U extends UnknownKeysParam = any> =
+  | z.ZodObject<any, U>
+  | z.ZodUnion<[TQuery<U>, ...TQuery<U>[]]>
+  | z.ZodIntersection<TQuery<U>, TQuery<U>>
+  | z.ZodDiscriminatedUnion<string, z.Primitive, z.ZodObject<any, U>>
+
+export function useValidatedQuery<T extends TQuery>(
+  event: CompatibilityEvent,
+  schema: T,
+) {
+  const query = useQuery(event)
+  const parsed = schema.safeParse(query)
+
+  if (!parsed.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: JSON.stringify(parsed.error.errors),
+    })
+  }
+
+  return parsed.data
+}
+
+export async function useValidatedBody<T extends TQuery>(
+  event: CompatibilityEvent,
+  schema: T,
+) {
+  const body = await useBody(event)
+  const parsed = schema.safeParse(body)
+
+  if (!parsed.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: JSON.stringify(parsed.error.errors),
+    })
+  }
+
+  return parsed.data
+}
+
+export {
+  z,
+}
