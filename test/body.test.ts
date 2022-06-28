74141acf606a072c99460500eb6dd708ad65a312
Robert Soriano <sorianorobertc@gmail.com>
2022-06-27 22:19:07 -0700

    test: add validation tests

---
+++ test/body.test.ts
@@ -0,0 +1,38 @@
+import type { SuperTest, Test } from 'supertest'
+import supertest from 'supertest'
+import { beforeEach, describe, expect, it } from 'vitest'
+import type { App, CompatibilityEvent } from 'h3'
+import { createApp } from 'h3'
+import { useValidatedBody, z } from '../src'
+
+describe('useValidatedBody', () => {
+  let app: App
+  let request: SuperTest<Test>
+
+  beforeEach(() => {
+    app = createApp({ debug: false })
+    request = supertest(app)
+  })
+
+  const bodySchema = z.object({
+    optional: z.string().optional(),
+    required: z.boolean(),
+  })
+
+  it('returns 200 OK if body matches validation schema', async () => {
+    app.use('/validate', async (req: CompatibilityEvent) => await useValidatedBody(req, bodySchema))
+
+    const res = await request.post('/validate').send({ required: true })
+
+    expect(res.status).toEqual(200)
+  })
+
+  it('throws 400 Bad Request if body does not match validation schema', async () => {
+    app.use('/validate', async (req: CompatibilityEvent) => await useValidatedBody(req, bodySchema))
+
+    const res = await request.post('/validate').send({})
+
+    expect(res.status).toEqual(400)
+    expect(res.body).toMatchSnapshot()
+  })
+})
---
+++ test/query.test.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import type { SuperTest, Test } from 'supertest'
+import supertest from 'supertest'
+import type { App, CompatibilityEvent } from 'h3'
+import { createApp } from 'h3'
+import { useValidatedQuery, z } from '../src'
+
+describe('useValidatedQuery', () => {
+  let app: App
+  let request: SuperTest<Test>
+
+  beforeEach(() => {
+    app = createApp({ debug: false })
+    request = supertest(app)
+  })
+
+  const querySchema = z.object({
+    required: z.string(),
+  })
+
+  it('returns 200 OK if query matches validation schema', async () => {
+    app.use('/validate', (req: CompatibilityEvent) => useValidatedQuery(req, querySchema))
+
+    const res = await request.get('/validate?required')
+
+    expect(res.status).toEqual(200)
+  })
+
+  it('throws 400 Bad Request if query does not match validation schema', async () => {
+    app.use('/validate', (req: CompatibilityEvent) => useValidatedQuery(req, querySchema))
+
+    const res = await request.get('/validate')
+
+    expect(res.status).toEqual(400)
+    expect(res.body).toMatchSnapshot()
+  })
+})
